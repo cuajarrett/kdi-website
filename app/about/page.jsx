2692c1eff40454bfdb704abe29e5b4ea9cd2f60c
cc-visionary <lim.christopher.go@gmail.com>
2025-01-17 01:14:55 +0800

    Skip Linting on Build

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { title } from "@/components/primitives";
 import { Image } from "@nextui-org/image";
 import { Accordion, AccordionItem } from "@nextui-org/accordion";
 import { Button } from "@nextui-org/button";
 
+import { title } from "@/components/primitives";
 import { ABOUT } from "@/data";
 
 export default function AboutPage() {
@@ -21,30 +21,30 @@ export default function AboutPage() {
       </section>
 
       {/* History Section */}
-      <section class="py-12 sm:py-16 lg:py-20 xl:py-24">
-        <div class="text-center space-y-4">
+      <section className="py-12 sm:py-16 lg:py-20 xl:py-24">
+        <div className="text-center space-y-4">
           <h2 className={title()}>History</h2>
-          <p class="mx-auto max-w-2xl text-lg font-normal text-gray-700 lg:text-xl lg:leading-8">
+          <p className="mx-auto max-w-2xl text-lg font-normal text-gray-700 lg:text-xl lg:leading-8">
             How it all started
           </p>
         </div>
-        <ul class="mx-auto mt-8 grid max-w-md grid-cols-1 gap-10 lg:max-w-7xl lg:grid-cols-3">
+        <ul className="mx-auto mt-8 grid max-w-md grid-cols-1 gap-10 lg:max-w-7xl lg:grid-cols-3">
           {historySection.map(({ description, image, year }, index) => (
-            <li class="flex-start group relative flex items-start lg:items-center lg:flex-col">
+            <li className="flex-start group relative flex items-start lg:items-center lg:flex-col">
               {index < historySection.length - 1 && (
                 <span
-                  class="absolute left-[18px] top-14 h-[calc(100%_-_32px)] w-px bg-gray-300 lg:right-0 lg:left-2/3 lg:top-[18px] lg:h-px lg:w-[calc(100%_-_72px)]"
+                  className="absolute left-[18px] top-14 h-[calc(100%_-_32px)] w-px bg-gray-300 lg:right-0 lg:left-2/3 lg:top-[18px] lg:h-px lg:w-[calc(100%_-_72px)]"
                   aria-hidden="true"
                 ></span>
               )}
-              <div class="inline-flex p-2 shrink-0 items-center justify-center rounded-xl border border-gray-300 bg-gray-50 transition-all duration-200 group-hover:text-white group-hover:border-gray-900 group-hover:bg-gray-900">
+              <div className="inline-flex p-2 shrink-0 items-center justify-center rounded-xl border border-gray-300 bg-gray-50 transition-all duration-200 group-hover:text-white group-hover:border-gray-900 group-hover:bg-gray-900">
                 {year}
                 {/* <svg
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
                     xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5 text-gray-600 group-hover:text-white"
+                    className="h-5 w-5 text-gray-600 group-hover:text-white"
                   >
                     <path
                       d="M21 12C21 13.6569 16.9706 15 12 15C7.02944 15 3 13.6569 3 12M21 5C21 6.65685 16.9706 8 12 8C7.02944 8 3 6.65685 3 5M21 5C21 3.34315 16.9706 2 12 2C7.02944 2 3 3.34315 3 5M21 5V19C21 20.6569 16.9706 22 12 22C7.02944 22 3 20.6569 3 19V5"
@@ -54,9 +54,9 @@ export default function AboutPage() {
                     ></path>
                   </svg> */}
               </div>
-              <div class="ml-6 lg:ml-0 lg:mt-10">
+              <div className="ml-6 lg:ml-0 lg:mt-10">
                 <Image src={image} />
-                <h4 class="mt-2 text-base text-gray-700 text-justify">
+                <h4 className="mt-2 text-base text-gray-700 text-justify">
                   {description}
                 </h4>
               </div>
--- app/showroom/page.jsx
+++ app/showroom/page.jsx
@@ -43,6 +43,7 @@ export default function ShowroomPage() {
         {/* Google Map Embed */}
         <div>
           <iframe
+            title="Google Map of Showroom"
             className="rounded-xl"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1930.7987473256312!2d121.01282339999999!3d14.564995199999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c9a079973353%3A0xe135b8071a27f513!2s7646%20Guijo%2C%20Makati%2C%201203%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1736960182680!5m2!1sen!2sph"
             width="100%"
