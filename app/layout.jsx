847d75dd4f81b3c3d5a4fedc750dc4bb6e100822
cc-visionary <lim.christopher.go@gmail.com>
2025-01-17 01:22:45 +0800

    Set default theme to light

--- app/layout.jsx
+++ app/layout.jsx
@@ -61,7 +61,7 @@ export default function RootLayout({ children }) {
           fontSans.variable
         )}
       >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+        <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
           <div className="relative flex flex-col h-screen">
             <Navbar />
             <main className="container mx-auto max-w-7xl py-8 px-6 flex-grow">
