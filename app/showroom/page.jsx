fd305cbc43402da7bc3f40c55bac2c4a8b10bf89
cc-visionary <lim.christopher.go@gmail.com>
2025-01-29 11:36:51 +0800

    Updated showroom google maps iframe to direct at the actual place

--- app/showroom/page.jsx
+++ app/showroom/page.jsx
@@ -45,7 +45,7 @@ export default function ShowroomPage() {
         <div>
           <iframe
             title="Google Map of Showroom"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1930.7987473256312!2d121.01282339999999!3d14.564995199999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c9a079973353%3A0xe135b8071a27f513!2s7646%20Guijo%2C%20Makati%2C%201203%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1736960182680!5m2!1sen!2sph"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3861.5974948978796!2d121.01024847601212!3d14.564995185917088!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c9256acfffff%3A0x70ee5c56f9f5d737!2sKassi%20Distributors%2C%20Inc.!5e0!3m2!1sen!2sph!4v1738121659700!5m2!1sen!2sph"
             width="100%"
             height="400"
             allowFullScreen=""
