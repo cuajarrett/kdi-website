be44abfb167ed47cf8dfffa33052064178b37d56
cuajarrett <jp.cua@kassidinc.com>
2025-02-15 23:11:39 +0800

    Update page.jsx

--- app/brands/page.jsx
+++ app/brands/page.jsx
@@ -5,7 +5,7 @@ import { BRANDS } from "@/data";
 export default function BrandsListPage() {
   return (
     <div>
-      <h1 className={title()}>Our Partners</h1>
+      <h1 className={title()}>Our Brands</h1>
       <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {BRANDS.map(({ name, logo, slug, description }, index) => (
           <Card
