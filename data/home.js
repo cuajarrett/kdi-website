d75205651f37f7bbfa5ae7f0b04ce28c9308619b
cuajarrett <jp.cua@kassidinc.com>
2025-02-27 16:37:21 +0800

    Update home.js

--- data/home.js
+++ data/home.js
@@ -27,7 +27,7 @@ export const HOME = {
     ),
     images: [
       {
-        desktopSrc: "/gallery/Home/Hero_HomePage1_Desktop.jpg",
+        desktopSrc: "/gallery/Home/Hero_HomePage1_Desktop.png",
         mobileSrc: "/gallery/Home/Hero_HomePage1_Mobile.webp",
         alt: "Kassi Distributors Inc. Gallery 1",
       },
